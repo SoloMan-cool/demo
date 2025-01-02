f176bbb5907d176e9835b040260f561d1ad7b170
SoloMAn-cool <soloman27179@gmail.com>
2025-01-02 18:28:12 +0500

    small changes x3

--- script.js
+++ script.js
@@ -3,20 +3,24 @@ let elBtn1 = document.querySelector('.mushroom')
 let elBtn2 = document.querySelector('.pepperoni')
 let elBtn3 = document.querySelector('.neapolitan')
 let elBtn4 = document.querySelector('.greek')
-    
+let elBtn = document.querySelector('.margarita')
+
+elBtn.addEventListener('click', function(){
+    elRecipe.classList.remove('none')
+})
 
 elBtn1.addEventListener('click', function(){
     elRecipe.classList.remove('none')
 })
-    
+
 elBtn2.addEventListener('click', function(){
     elRecipe.classList.remove('none')
 })
-    
+
 elBtn3.addEventListener('click', function(){
     elRecipe.classList.remove('none')
 })
-    
+
 elBtn4.addEventListener('click', function(){
     elRecipe.classList.remove('none')
 })
