45dfca487d789f82ecb327f8dae45bfc290c28b2
SoloMAn-cool <soloman27179@gmail.com>
2025-01-06 20:06:24 +0500

    for me textContent does not work (I do not know why)

--- script.js
+++ script.js
@@ -30,3 +30,35 @@ let elBtn5 = elRecipe.querySelector('.recipe__btn')
 elBtn5.addEventListener('click', function(){
     elRecipe.classList.add('none')
 })
+
+let elQalinlik = document.querySelector('.savat__text4')
+
+let elSelect = document.querySelector('.dough__btn')
+let elBox = document.querySelector('.dough__box')
+let elQalin = document.querySelector('.qalin')
+let elOrtacha = document.querySelector('.ortancha')
+let elYupqa = document.querySelector('.yupqa')
+let elText = document.querySelector('.dough__text')
+let elDough = document.querySelector('.dough')
+
+elSelect.addEventListener('click', function(){
+    elBox.classList.toggle('none')
+    elQalin.classList.toggle('none')
+    elOrtacha.classList.toggle('none')
+    elYupqa.classList.toggle('none')
+})
+
+elQalin.addEventListener('click', function(){
+    elDough.classList.add('none')
+    elQalinlik.textContent('Hamirni qalinligi: Qalin')
+})
+
+elOrtacha.addEventListener('click', function(){
+    elDough.classList.add('none')
+    elQalinlik.textContent('Hamirni qalinligi: Ortancha')
+})
+
+elYupqa.addEventListener('click', function(){
+    elDough.classList.add('none')
+    elQalinlik.textContent('Hamirni qalinligi: Yupqa')
+})
